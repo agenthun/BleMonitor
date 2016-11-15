242c936ff9e276d1a3e4ae9d0bd955f767135997
agenthun <hun333@126.com>
2016-11-15 17:51:07 +0800

    fixed BLE @ API 23权限问题

--- app/src/main/java/com/agenthun/blemonitor/activity/MainActivity.java
+++ app/src/main/java/com/agenthun/blemonitor/activity/MainActivity.java
@@ -2,8 +2,10 @@ package com.agenthun.blemonitor.activity;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -52,8 +54,6 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, ScanDeviceFragment.newInstance()).commit();
         }
-
-        getPersimmions();
     }
 
     @Override
@@ -95,28 +95,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        getPersimmions();
+        getPermissions(MainActivity.this);
     }
 
-    private void getPersimmions() {
+    private void getPermissions(Context context) {
         List<String> permissions = new ArrayList<>();
 
+        // 蓝牙权限必须开启ACCESS_COARSE_LOCATIO或ACCESS_FINE_LOCATION权限，用户如果禁止，则每次进入都会申请
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
         // 读写外设为必须权限，用户如果禁止，则每次进入都会申请
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         }
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
 
         if (permissions.size() > 0) {
-            ActivityCompat.requestPermissions(MainActivity.this, permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            ActivityCompat.requestPermissions((Activity) context, permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
         }
     }
 
-    private boolean addPermission(List<String> permissionsList, String permission) {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+    private boolean addPermission(Context context, List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, permission)) {
                 return true;
             } else {
                 permissionsList.add(permission);
